7900deba90c5280f90495c108b79849f98123f33
Sebastian Bachmann <bachmann.s@ikarus.at>
2019-08-22 14:02:31 +0200

    getting the build to build

--- setup.py
+++ setup.py
@@ -40,10 +40,29 @@ setup(
                      'elsim/elsign/aho_corasick.c',
                      'elsim/elsign/cluster.c',
                      'elsim/elsign/formula.cc',
+
+                     # FIXME: this is a crude hack to get it to work!!!
+                     # idealy, one would link against the other library, but this seems to be very hard to do...
+                     'elsim/similarity/similarity.c',
+                     'elsim/similarity/bz2/bz2.c',
+                     'elsim/similarity/lzma/Alloc.c',
+                     'elsim/similarity/lzma/LzFind.c',
+                     'elsim/similarity/lzma/LzmaDec.c',
+                     'elsim/similarity/lzma/LzmaEnc.c',
+                     'elsim/similarity/lzma/LzmaLib.c',
+                     'elsim/similarity/lzma/lzma.c',
+                     'elsim/similarity/smaz/smaz.c',
+                     'elsim/similarity/snappy/snappy.cc',
+                     'elsim/similarity/vcblocksort/vcblocksort.c',
+                     'elsim/similarity/xz/xz.c',
+                     'elsim/similarity/z/z.c',
                     ],
-            libraries=['muparser'],
+            libraries=['muparser', 'lzma', 'snappy', 'bz2', 'z'],
             include_dirs=['elsim/similarity'],
-            extra_compile_args=['-D_GLIBCXX_PERMIT_BACKWARD_HASH']
+            extra_compile_args=[
+                '-D_GLIBCXX_PERMIT_BACKWARD_HASH',  # required for sparsehash
+                '-D_7ZIP_ST',  # required for LZMA
+                ],
         )
     ],
 
